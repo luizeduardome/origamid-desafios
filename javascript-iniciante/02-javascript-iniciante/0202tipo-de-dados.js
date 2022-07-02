4e33fed2996b87ddbba4bda02cef49ce807d50ed
Luiz Eduardo <luizefm@al.insper.edu.br>
2022-07-02 19:34:03 -0300

    Feito o exercício do vídeo 0202tipo-de-dados.js.

--- javascript-iniciante/02-javascript-iniciante/0202tipo-de-dados.js
+++ javascript-iniciante/02-javascript-iniciante/0202tipo-de-dados.js
@@ -31,3 +31,46 @@ var gol = 1000;
 var frase = 'Romário fez ' + gol + ' gols';
 console.log(frase);
 console.log(typeof frase);
+
+
+// Usando backlashs
+var frase1 = "Esse é o \"melhor\" jogo do Palmeiras" // as barras invertidas neutralizam a função das aspas duplas na string.
+console.log(frase1);
+
+// Passando string diretamente no console.log()
+console.log("O Brasil é o melhor país do mundo!");
+
+// Template string
+
+var gols = 1000;
+var frase2 = "Romário fez " + gols + " gols";
+var frase3 = `Romário fez ${gols} gols`; // lembrete: Utilizar a crase (`)
+console.log(frase3)
+
+/*Exercício*/
+
+// Declare uma variável contendo uma string 
+var lanche = 'sandwich';
+console.log(lanche);
+
+// Declare uma variável contendo um número dentro de uma string
+var fraseCompleta = `O Brasil foi descoberto em ${1500} pelos portugueses`
+console.log(fraseCompleta);
+
+// Declare uma variável com sua idade
+var age1 = 37;
+console.log(age1);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var primeiroNome1 = 'Luiz Eduardo';
+var sobreNome1 = 'de Figueiredo Menezes';
+var nomeCompleto1 = primeiroNome1 + ' ' + sobreNome1;
+console.log(nomeCompleto1);
+
+// Coloque a seguinte frase em uma variável: It's time
+var english = 'It\'\s time'
+console.log(english);
+
+// Verifique o tipo da variável que contem o seu nome
+console.log(typeof nomeCompleto1);
