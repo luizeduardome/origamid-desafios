6de02b218d8528e2f28f7baa34eab246402f9c2b
Luiz Eduardo <luizefm@al.insper.edu.br>
2022-07-02 19:07:50 -0300

    Iniciado a aula 02 "vídeo 0202-tipo-de-dados".

---
+++ javascript-iniciante/02-javascript-iniciante/0202tipo-de-dados.js
@@ -0,0 +1,18 @@
+/* 7 tipos de dados*/
+
+// todos são primitivos exceto os objetos
+
+var nome = "Luiz Eduardo"; //string
+var idade = 37; //number
+var possuiFaculdadeSim = true; //boolean
+var time; //undefined
+var comida = null; //null
+var simbolo = Symbol(); //symbol
+var novoObjeto = {}; //objeto
+//Primitivos são imutaveis
+
+
+// descobrindo o tipo da variável
+console.log(typeof nome); //string
+
+console.log(typeof time); //undefine
