7b6790d28aeb1c139c4cd38bd7b664e4ab6c4c28
Luiz Eduardo <luizefm@al.insper.edu.br>
2022-07-02 18:29:50 -0300

    Aprendendo o básico sobre variáveis em js.

---
+++ javascript-iniciante/02-javascript-iniciante/0201variaveis.js
@@ -0,0 +1,21 @@
+/* Utilizando uma variável para cada valor.*/
+
+var nome = 'Luiz Eduardo';
+let idade = 37;
+const possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+/*Declarando uma variável para vários valores.*/
+
+var sobreNome = 'Menezes', // a separação ocorre por vírgulas
+cidade = 'Belo Horizonte';
+
+
+console.log(sobreNome, cidade);
