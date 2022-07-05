f436608d4f9e424677d804d27aa6409c2accacc9
Luiz Eduardo <luizefm@al.insper.edu.br>
2022-07-05 18:16:04 -0300

    Operadores aritméticos.

--- javascript-iniciante/02-javascript-iniciante/0203numeros-operadores1.js
+++ javascript-iniciante/02-javascript-iniciante/0203numeros-operadores1.js
@@ -5,3 +5,15 @@ var gols = 1000;
 var pi = 3.14; // ponto para decimal
 var exp = 2e10; // 20000000000
 
+// Precisão de até 15 dígitos
+
+/* Operdores Aritméticos */
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplica = 100 * 2; // 200
+var divisão = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+// Lembrando que soma '+' em Strings
+// serve para concatenar
