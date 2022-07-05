c013f326d64e84672bf64628b96a2c4fab412060
Luiz Eduardo <luizefm@al.insper.edu.br>
2022-07-05 18:19:35 -0300

    Operadores Aritméticos (Strings).

--- javascript-iniciante/02-javascript-iniciante/0203numeros-operadores1.js
+++ javascript-iniciante/02-javascript-iniciante/0203numeros-operadores1.js
@@ -17,3 +17,14 @@ var modulo = 14 % 5; // 4
 
 // Lembrando que soma '+' em Strings
 // serve para concatenar
+
+
+/* Operadores Aritméticos (Strings) */
+var soma = '100' + 50; //10050
+var subtracao = '100' - 50; // 50
+var multiplicacao = '100' * '2'; // 200
+var divisão = 'Comprei 10' / 2; // Nan (Not a Number)
+
+// É possivel verificar se uma
+// variável é Nan ou não com a
+// função 'isNaN()'
