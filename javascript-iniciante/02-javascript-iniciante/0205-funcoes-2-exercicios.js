2191eed22a39990edae2d8353fdb7b038b751012
Luiz Eduardo <luizefm@al.insper.edu.br>
2022-08-29 15:10:00 -0300

    0205-funcoes-2-exercicios

---
+++ javascript-iniciante/02-javascript-iniciante/0205-funcoes-2-exercicios.js
@@ -0,0 +1,52 @@
+// Crie uma função para verificar se um valor é Truthy
+
+function verificaValor(cor) {
+  if(cor === 'azul') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+function perimetroQuad(perimetro) {
+  return perimetro * perimetro;
+}
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+function meusDados() {
+  const nome = 'Luiz Eduardo';
+  const sobrenome = 'de Figueiredo Menezes';
+  return `${nome + sobrenome}`;
+}
+
+function meusDados2(nome, sobrenome) {
+  return nome + sobrenome;
+}
+
+// Crie uma função que verifica se um número é par
+
+function verificaPar()
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+
+// Corrija o erro abaixo
+// function precisoVisitar(paisesVisitados) {
+//   var totalPaises = 193;
+//   return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+// }
+// function jaVisitei(paisesVisitados) {
+//   return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+// }
+// precisoVisitar(20);
+// jaVisitei(20);
--- javascript-iniciante/02-javascript-iniciante/0205-funcoes-2.js
+++ javascript-iniciante/02-javascript-iniciante/0205-funcoes-2.js
@@ -12,3 +12,55 @@ function imc2(peso1, altura1) {
 
 imc2(20, 1.8);
 console.log(imc2(1000, 1.8));
+
+/* Valores retornados */
+
+/* 
+Uma função pode retornar qualquer tipo de dado e até outras
+funções. */
+
+function terceiraIdade(idade) {
+  console.log(typeof idade);
+  if(typeof idade !== 'number') {
+    return 'Informe a sua idade';
+  } else if(idade >= 60) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(terceiraIdade(60))
+/* ALERTA */
+/* Cuidado, retornar diferentes
+tipos de dados na mesma função
+não é uma boa Ideia. */
+
+/* ESCOPO */ 
+/* 
+Variáveis e funções definidas dentro de um bloco {}, não são
+visíveis fora delete. */
+var totalPaises = 193;
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países`;
+}
+
+/* ESCOPO LÉXICO */
+/* 
+Funções conseguem acessar variáveis que foram criadas no contexto 'pai' 
+*/
+
+var profissão = 'Engenheiro';
+
+function dados() {
+  var nome = 'Luiz';
+  var idade = 29;
+  function outrosDados() {
+    var endereco = 'Vitória / ES'
+    var idade = 37;
+    return `${nome}, ${idade}, ${endereco}, ${profissão}`;
+  }
+  return outrosDados();
+}
+
+// dados(); Retorna 'Luiz, 37, Vitória / ES, Engenheiro.'
+// outrosDados(); Retorna um erro
