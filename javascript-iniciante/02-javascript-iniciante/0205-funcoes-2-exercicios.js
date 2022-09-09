f4de5417409318b8ced1d83cd05e988846c6c4c1
Luiz Eduardo <luizefm@al.insper.edu.br>
2022-09-09 15:04:43 -0300

    0206-objetos-1

--- javascript-iniciante/02-javascript-iniciante/0205-funcoes-2-exercicios.js
+++ javascript-iniciante/02-javascript-iniciante/0205-funcoes-2-exercicios.js
@@ -1,18 +1,14 @@
 // Crie uma função para verificar se um valor é Truthy
 
-function verificaValor(cor) {
-  if(cor === 'azul') {
-    return true;
-  } else {
-    return false;
-  }
+function verificaValor(dado) {
+  return !!dado;
 }
 
 // Crie uma função matemática que retorne o perímetro de um quadrado
 // lembrando: perímetro é a soma dos quatro lados do quadrado
 
 function perimetroQuad(perimetro) {
-  return perimetro * perimetro;
+  return perimetro * 4;
 }
 
 // Crie uma função que retorne o seu nome completo
@@ -30,23 +26,38 @@ function meusDados2(nome, sobrenome) {
 
 // Crie uma função que verifica se um número é par
 
-function verificaPar()
+function verificaPar(number) {
+  if(number % 2 === 0) {
+    return 'par';
+  } else {
+    return 'ímpar';
+  }
+};
 
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
+
+function checkType(value) {
+  return typeof value;
+}
 
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
 // quando o evento 'scroll' ocorrer.
 
+addEventListener('click', function() {
+  console.log('Luiz Eduardo de Figueiredo Menezes');
+});
+
 // Corrija o erro abaixo
-// function precisoVisitar(paisesVisitados) {
-//   var totalPaises = 193;
-//   return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
-// }
-// function jaVisitei(paisesVisitados) {
-//   return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
-// }
-// precisoVisitar(20);
-// jaVisitei(20);
+var totalPaises = 193;
+
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+precisoVisitar(20);
+jaVisitei(20);
